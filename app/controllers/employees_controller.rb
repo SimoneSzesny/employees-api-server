055c3b82ae1eff43c18f894d7161ad212bd3faba
Simone <simoneszesny@gmail.com>
2017-04-04 18:37:01 -0500

    add employees API update action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,9 +3,33 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
     render "index.json.jbuilder"
   end
+
+  def create
+    @employee = Employee.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      birthdate: params[:birthdate],
+      email: params[:email],
+      ssn: params[:ssn]
+      )
+    @employee.save
+    redirect_to "/employees/#{@employee.id}"
+  end
   
   def show
     @employee = Employee.find_by(id: params[:id])
     render "show.json.jbuilder"
+ end
+
+ def update
+  @employee = Employee.find_by(id: params[:my_id])
+  @employee.first_name = params[:first_name] || @employee.first_name
+  @employee.last_name = params[:last_name] || @employee.last_name
+  @employee.email = params[:email] || @employee.email
+  @employee.birthdate = params[:birthdate] || @employee.birthdate
+  @employee.ssn = params[:ssn] || @employee.ssn
+  @employee.save
+  # redirect_to '/employees/#{@employee.id}'
+  render "show.json.jbuilder"
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
- get "/employees" => "employees#index"
+get "/employees" => "employees#index"
+post "/employees/:id" => "employees#create"
 get "/employees/:id" => "employees#show"
+patch "/employees/:id" => "employees#update"
 
 end
